9bb1863e29fd782bd9ced016d777ed1905296df3
Stu Zhao <z12y12l12@gmail.com>
2011-03-02 16:27:47 -0500

    Updated the licence string in source code to GPLv3

--- feedesk/feeds.py
+++ feedesk/feeds.py
@@ -2,11 +2,13 @@
 
 """
 
-__licence__ = """Copyright (c) 2009 Yanglei Zhao. All rights reserved.
+__licence__ = """Copyright (C) 2011  Yanglei Zhao. All right reserved.
+http://www4.ncsu.edu/~yzhao11/
+z12y12l12@gmail.com
 
-This program is free software; you can redistribute it and/or modify
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -15,10 +17,9 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-MA 02110-1301, USA.
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 __author__ = 'Yanglei Zhao <z12y12l12 [AT] gmail [DOT] com>'
 
 from feedparser import feedparser
